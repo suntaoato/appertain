e143e197a417c4151886c2f199fae8784b626e89
Howard Beard-Marlowe <howardbm@live.se>
2014-08-08 14:46:39 +0200

    More neatess, revise includes so the format is the same

--- tests/conf_Test.php
+++ tests/conf_Test.php
@@ -1,25 +1,24 @@
 <?php
 
 //Include conf class so we can use the functions
-$path = "src/classes/conf/conf.php";
-include_once($path);
+include_once(__DIR__ . "/../src/classes/conf/conf.php");
 
 class conf_Test extends PHPUnit_Framework_TestCase
-{	
-   public function testsCanBeRead()
-   {
+{
+	public function testsCanBeRead()
+	{
       $conf  = new Conf(__DIR__ . "/config.ini");
       $value = $conf->getKey("test","testKey");
-
-      print("Value fetched: " . $value . PHP_EOL);
-
-      $this->assertEquals("TestValue", $value);		
-   }
 	
-   public function testsMainCanBeRead()
-   {
-      $conf  = new Conf(__DIR__ . "/../src/config.ini");
-      $value =  $conf->getKey("db_config","host");
+		print("Value fetched: " . $value . PHP_EOL);
+
+		$this->assertEquals("TestValue", $value);		
+	}
+	
+	public function testsMainCanBeRead()
+	{
+		$conf  = new Conf(__DIR__ . "/../src/config.ini");
+		$value =  $conf->getKey("db_config","host");
       
       print("Value fetched: " . $value . PHP_EOL);
 
