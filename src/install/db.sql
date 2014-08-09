977e4ff7ce9692b2f47750297493679ef5ae36f9
root <murmudamus@gmail.com>
2014-08-09 18:04:25 +0200

    added vwEnabledProfiles view

--- src/install/db.sql
+++ src/install/db.sql
@@ -164,6 +164,7 @@ CREATE TABLE `profile_content` (
 
 LOCK TABLES `profile_content` WRITE;
 /*!40000 ALTER TABLE `profile_content` DISABLE KEYS */;
+INSERT INTO `profile_content` VALUES (1,4),(2,5),(3,6),(4,7),(5,8);
 /*!40000 ALTER TABLE `profile_content` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -405,8 +406,44 @@ INSERT INTO `users` VALUES (1,'admin','',1),(2,'howie@sweating.com','',1),(3,'ro
 UNLOCK TABLES;
 
 --
+-- Temporary table structure for view `vwEnabledProfiles`
+--
+
+DROP TABLE IF EXISTS `vwEnabledProfiles`;
+/*!50001 DROP VIEW IF EXISTS `vwEnabledProfiles`*/;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+/*!50001 CREATE TABLE `vwEnabledProfiles` (
+  `id` tinyint NOT NULL,
+  `name` tinyint NOT NULL,
+  `handle` tinyint NOT NULL,
+  `email` tinyint NOT NULL,
+  `path` tinyint NOT NULL
+) ENGINE=MyISAM */;
+SET character_set_client = @saved_cs_client;
+
+--
 -- Dumping routines for database 'appertain'
 --
+
+--
+-- Final view structure for view `vwEnabledProfiles`
+--
+
+/*!50001 DROP TABLE IF EXISTS `vwEnabledProfiles`*/;
+/*!50001 DROP VIEW IF EXISTS `vwEnabledProfiles`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8 */;
+/*!50001 SET character_set_results     = utf8 */;
+/*!50001 SET collation_connection      = utf8_general_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `vwEnabledProfiles` AS select `p`.`id` AS `id`,`p`.`name` AS `name`,`p`.`handle` AS `handle`,`p`.`email` AS `email`,`c`.`path` AS `path` from (((`users` `u` join `profiles` `p` on((`p`.`user_id` = `u`.`id`))) join `profile_content` `pc` on((`p`.`id` = `pc`.`profile_id`))) join `content` `c` on((`c`.`id` = `pc`.`content_id`))) where (`u`.`status` = 1) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -417,4 +454,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-08-09 17:52:36
+-- Dump completed on 2014-08-09 18:03:07
