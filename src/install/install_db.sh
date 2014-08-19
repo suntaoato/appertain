49137aa773ebf1a3e639483684c0a6e12cce527a
root <murmudamus@gmail.com>
2014-08-19 12:56:27 +0200

    modified install_db script to recreate db

--- src/install/install_db.sh
+++ src/install/install_db.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-for f in *.sql;
+## This is just a simple helper script to set up db while in development
+## Replace this with proper setups
+echo "Recreating Schema";
+mysql -u root -ppassword < ./appertain_schema.sql
+echo "Done";
+for t in tables/*.sql;
  do
-  echo "${f}";
-  mysql -u root -ppassword appertain < "${f}";
+  echo "${t}";
+  mysql -u root -ppassword appertain < "${t}";
+done
+for r in routines/*.sql;
+ do
+  echo "${r}";
+  mysql -u root -ppassword appertain < "${r}";
 done
